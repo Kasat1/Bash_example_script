194550f999218578ba4f24359af9193ff1952772
Kasat1 <99502652+Kasat1@users.noreply.github.com>
2022-07-05 15:37:27 +0300

    Create Script.sh

---
+++ Script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Write a function (and write its code in the answer) that takes n words as input,
+# and returns a sentence in which these words are sorted by the number of vowels.
+function create_array {
+     arr=()
+     IFS=' ' read -r -a array_str<<<$str
+     for element in "${array_str[@]}"
+          do
+          number=$( echo "$element"| grep -o '[aeiouAEIOU]' | wc -l )
+          arr+=($number-$element)
+     done
+}
+
+
+str="qweruioioioioi qweioioi qwertyuia qweIAIA"
+create_array $str
+
+echo "${arr[@]}" | tr ' ' '\n' | sort -rn | tr -d '[0-9]-' | tr '\n' ' '
